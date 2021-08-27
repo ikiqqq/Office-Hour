085da948a23d4cb0b157b141277fcea8eb904c7e
ikiqqq <riskidarliana76@gmail.com>
2021-08-27 20:49:31 +0700

    boolean

---
+++ GA_Code_Challenges-master/Month 1/Week 2/Day 2 - Convert Boolean Values/solution.js
@@ -0,0 +1,16 @@
+function boolToWord( bool ){
+    if (bool == true)
+      return "Yes"
+    else bool == false
+          return "No"
+  }
+
+  //best solution
+
+  function boolToWord( bool ){
+    return bool ? 'Yes':'No';
+  }
+
+  const boolToWord = bool => bool ? 'Yes' : 'No';
+
+  
