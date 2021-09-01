978da7b22c21d73286dbfdd5da752cf35c55951d
ikiqqq <riskidarliana76@gmail.com>
2021-09-01 19:25:36 +0700

    intervew challenge

--- GA_Code_Challenges-master/Month 1/Week 2/Day 3 - Remove String Spaces/solution.js
+++ GA_Code_Challenges-master/Month 1/Week 2/Day 3 - Remove String Spaces/solution.js
@@ -4,3 +4,9 @@ const myArr = str.split(" ");
 function noSpace(str){
     return str.split(' ').join('');
     }
+
+    //best solution
+
+    function noSpace(x){
+        return x.replace(/\s/g, '');
+      }
