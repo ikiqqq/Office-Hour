fbbbee8c7c0167347938a02f480e7f1c712b238a
ikiqqq <riskidarliana76@gmail.com>
2021-09-01 20:54:59 +0700

    intervew challenge

--- GA_Code_Challenges-master/Month 1/Week 2/Day 4 - Find The Smallest Integer/solution.js
+++ GA_Code_Challenges-master/Month 1/Week 2/Day 4 - Find The Smallest Integer/solution.js
@@ -12,3 +12,24 @@ class SmallestIntegerFinder {
 
 //   best solution
 
+function findSmallestInt(args) {
+    let smallest = args[0]
+
+    args.forEach(i => {
+        if(i < smallest) smallest = i
+    })
+
+    return smallest
+}
+
+
+function findSmallestInt(args) {
+    return Math.min(...args)
+}
+
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min.apply(null, args);
+    }
+  }
