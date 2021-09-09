398396f110300e19c36847971412cebff69db3dc
ikiqqq <riskidarliana76@gmail.com>
2021-09-09 20:56:25 +0700

    Parse Nice Int from char problem

--- GA_Code_Challenges-master/Month 1/Week 3/Day 3 - Parse Nice Int/solution.js
+++ GA_Code_Challenges-master/Month 1/Week 3/Day 3 - Parse Nice Int/solution.js
@@ -2,3 +2,23 @@ function getAge(inputString){
     // return correct age (int). Happy coding :) 
       return Number(inputString[0]);
     }
+
+    //logic
+    function getAge(inputString){
+      // return correct age (int). Happy coding :) 
+        let numbers = ["0","1","2","3","4","5","6","7","8","9"]
+        
+        for (let i = 0; i < inputString.length;i++) {
+          if (numbers.includes(inputString[i])) {
+            return Number(inputString[i])
+          }
+        }
+        
+        return "No Number in Sentence"
+      }
+
+      //best solution
+      function getAge(inputString){
+        // return correct age (int). Happy coding :) 
+          return parseInt(inputString);
+        }
