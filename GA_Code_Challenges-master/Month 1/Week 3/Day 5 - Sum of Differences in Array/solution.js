50a76cca2c5040a32b388a577d7ee78b31307d1e
ikiqqq <riskidarliana76@gmail.com>
2021-09-14 21:04:56 +0700

    m difference array

--- GA_Code_Challenges-master/Month 1/Week 3/Day 5 - Sum of Differences in Array/solution.js
+++ GA_Code_Challenges-master/Month 1/Week 3/Day 5 - Sum of Differences in Array/solution.js
@@ -1,4 +1,4 @@
-function sumOfDifferences(arr) {
+/*function sumOfDifferences(arr) {
     if (arr.length < 1) {
     return 0;
     } else if (arr.length > 1) {
@@ -7,4 +7,19 @@ function sumOfDifferences(arr) {
     } else {
     return arr[0] - arr[1];
     }
-}
+}*/ //belum jalan kode nya
+
+
+//solution
+
+function sumOfDifferences(arr) {
+    // Sort dulu, bikin sort desc, biar gampang cari selisihnya
+    // Bikin variabel base utk hitung sum nya, start 0.
+    // Bikin loop yg ngereturn jumlah dari selisih nilai index i dan i+1, dst
+    let sum = 0;
+    let sortedArr = arr.sort((a, b) => b - a)
+    for (let i = 0; i < sortedArr.length - 1; i++) {
+      sum += (arr[i] - arr[i + 1]);
+    }
+    return sum;
+  }
