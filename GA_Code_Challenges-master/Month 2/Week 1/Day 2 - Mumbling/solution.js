9043600685ec44582b1c5afd6e64506b3ac01f27
ikiqqq <riskidarliana76@gmail.com>
2021-09-21 21:00:59 +0700

    mumbling

--- GA_Code_Challenges-master/Month 2/Week 1/Day 2 - Mumbling/solution.js
+++ GA_Code_Challenges-master/Month 2/Week 1/Day 2 - Mumbling/solution.js
@@ -3,9 +3,9 @@ function accum(s) {
   let arr = []; //kita jadiin array
   
   for (let i = 0; i < s.length; i++) {
-    let result = '';
+    let result = ''; // untuk nambah index
     
-    for (let j = 0; j < i + 1; j++) {
+    for (let j = 0; j < i + 1; j++) { // untuk ngepush
       result += j == 0 ? s[i].toUpperCase() : s[i].toLowerCase();
     }
     arr.push(result);
