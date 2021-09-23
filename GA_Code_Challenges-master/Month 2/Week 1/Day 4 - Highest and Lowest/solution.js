92f236e98af58792fba1d203c7d2a8af3ed79ff9
ikiqqq <riskidarliana76@gmail.com>
2021-09-23 21:01:26 +0700

    highest and lowest

--- GA_Code_Challenges-master/Month 2/Week 1/Day 4 - Highest and Lowest/solution.js
+++ GA_Code_Challenges-master/Month 2/Week 1/Day 4 - Highest and Lowest/solution.js
@@ -1,6 +1,8 @@
 function highAndLow(numbers){
-    // ...
-    var temp = numbers.split(' ');
-    temp.sort((a,b)=>{return a-b; });
+  // ...
+   let temp = numbers.split(' ');
+    temp.sort((a,b)=>{
+      return a-b
+    });
     return  temp[temp.length-1] + ' ' + temp[0]
-  }
+}
